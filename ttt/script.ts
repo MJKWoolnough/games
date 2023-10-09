28150c916ed0365072f7e23686879aef8960f571
Michael Woolnough <michael.woolnough@gmail.com>
2023-10-09 07:02:44 +0100

    layout board pieces

--- ttt/script.ts
+++ ttt/script.ts
@@ -1,7 +1,7 @@
 import {add, render} from './lib/css.js';
 import {amendNode, clearNode} from './lib/dom.js';
 import ready from './lib/load.js';
-import {circle, defs, line, path, svg} from './lib/svg.js';
+import {circle, defs, g, line, path, rect, svg, use} from './lib/svg.js';
 
 ready.then(() => {
 	add({
@@ -13,16 +13,23 @@ ready.then(() => {
 
 	amendNode(document.head, render());
 
-	const board = svg({"viewBox": "0 0 99 99", "stroke": "#000"}, [
+	const clicked = (_: number) => {},
+	      cells = Array.from({"length": 9}, (_, n) => g({"transform": `translate(${(n % 3) * 33} ${Math.floor(n / 3) * 33})`}, [
+		use({"href": "#X"}),
+		use({"href": "#O"}),
+		rect({"onclick": () => clicked(n)})
+	      ])),
+	      board = svg({"viewBox": "0 0 99 99", "stroke": "#000"}, [
 		defs([
 			path({"id": "X", "d": "m5,5 l23,23 m0,-23 l-23,23", "stroke-width": 2}),
 			circle({"id": "O", "cx": 49, "cy": 16, "r": 13, "stroke-width": 2, "fill": "none"})
 		]),
+		cells,
 		line({"x1": 33, "x2": 33, "y2": 99}),
 		line({"x1": 66, "x2": 66, "y2": 99}),
 		line({"y1": 33, "x2": 99, "y2": 33}),
-		line({"y1": 66, "x2": 99, "y2": 66}),
-	]);
+		line({"y1": 66, "x2": 99, "y2": 66})
+	      ]);
 
 	clearNode(document.body, board);
 });
