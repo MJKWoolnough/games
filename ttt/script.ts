3c6e070acc32418faf9ce53707682fed03186605
Michael Woolnough <michael.woolnough@gmail.com>
2023-10-10 21:21:45 +0100

    restart game on click

--- ttt/script.ts
+++ ttt/script.ts
@@ -1,5 +1,5 @@
 import {add, at, render} from './lib/css.js';
-import {amendNode, clearNode} from './lib/dom.js';
+import {amendNode, clearNode, event, eventOnce} from './lib/dom.js';
 import ready from './lib/load.js';
 import {circle, defs, g, line, path, rect, svg, text, use} from './lib/svg.js';
 
@@ -111,7 +111,7 @@ ready.then(() => {
 
 	const start = (t: number) => {
 		for(const cell of cells) {
-			amendNode(cell, {"class": ["!X", "!Y"]});
+			amendNode(cell, {"class": ["!X", "!O"]});
 		}
 
 		for (const win of winLines) {
@@ -120,6 +120,7 @@ ready.then(() => {
 
 		game.fill(0);
 
+		amendNode(status, {"class": ""});
 		setMark(board, turn = t);
 	      },
 	      setMark = (e: SVGElement, t: number) => amendNode(e, {"class": "C " + (t === 1 ? "X" : t === 2 ? "O" : "")}),
@@ -132,9 +133,11 @@ ready.then(() => {
 
 		const win = isWin();
 		if (win >= 0) {
-			amendNode(winLines[win], {"style": "display: unset"});
+			const next = -turn + 3;
 
+			amendNode(winLines[win], {"style": "display: unset"});
 			amendNode(status, {"class": turn === 1 ? "X" : "O"});
+			amendNode(restart, {"onclick": event(() => start(next), eventOnce)});
 
 			turn = 3;
 		}
@@ -175,15 +178,16 @@ ready.then(() => {
 		line({"class": "W", "x1": 16, "y1": 16, "x2": 83, "y2": 83}),
 		line({"class": "W", "x1": 83, "y1": 16, "x2": 16, "y2": 83})
 	      ]).flat(),
-	      game = Array.from({"length": 9}, () => 0),
+	      restart = rect({"x": -28, "y": 18, "width": 56, "height": 17, "style": {"fill": "#888", "border": "2px outset #000"}}),
 	      status = g({"id": "S", "text-anchor": "middle", "dominant-baseline": "hanging", "transform": "translate(49,30)"}, [
 		rect({"x": -28, "width": 56, "height": 20, "fill": "#f00"}),
 		text("X wins!"),
 		text("O wins!"),
 		text("Draw!"),
-		rect({"x": -28, "y": 18, "width": 56, "height": 17, "style": {"fill": "#888", "border": "2px outset #000"}}),
+		restart,
 		text({"y": 20}, "Restart"),
 	      ]),
+	      game = Array.from({"length": 9}, () => 0),
 	      board = svg({"viewBox": "0 0 99 99"}, [
 		defs([
 			path({"id": "X", "d": "m5,5 l23,23 m0,-23 l-23,23", "stroke-width": 2}),
