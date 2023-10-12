3d65fde3f52853e50a937baeb3cb8b6136cc7463
Michael Woolnough <michael.woolnough@gmail.com>
2023-10-12 07:01:28 +0100

    move click clode up a level

--- flipit/script.ts
+++ flipit/script.ts
@@ -57,42 +57,46 @@ ready.then(() => {
 
 	amendNode(document.head, render());
 
+	let level = 0;
+
 	const scale = 200,
 	      board = svg({"viewBox": `0 0 ${scale} ${scale}`}),
 	      game: boolean[] = [],
 	      cells: SVGRectElement[] = [],
-	      start = (level: number) => {
-		const cellSize = scale / level,
-		      clicked = (n: number) => {
-			const x = n % level,
-			      y = Math.floor(n / level),
-			      toChange = [n];
+	      clicked = (n: number) => {
+		const x = n % level,
+		      y = Math.floor(n / level),
+		      toChange = [n];
 
-			if (x > 0) {
-				toChange.push(n-1);
-			}
+		if (x > 0) {
+			toChange.push(n-1);
+		}
 
-			if (x < level - 1) {
-				toChange.push(n+1);
-			}
+		if (x < level - 1) {
+			toChange.push(n+1);
+		}
 
-			if (y > 0) {
-				toChange.push(n-level);
-			}
+		if (y > 0) {
+			toChange.push(n-level);
+		}
 
-			if (y < level - 1) {
-				toChange.push(n+level);
-			}
+		if (y < level - 1) {
+			toChange.push(n+level);
+		}
 
-			for (const c of toChange) {
-				amendNode(cells[c], {"class": {"S": game[c] = !game[c]}});
-			}
+		for (const c of toChange) {
+			amendNode(cells[c], {"class": {"S": game[c] = !game[c]}});
+		}
 
 
-			if (game.every(c => c)) {
-				start(level+1);
-			}
-		      };
+		if (game.every(c => c)) {
+			start(level+1);
+		}
+	      },
+	      start = (l: number) => {
+		level = l;
+
+		const cellSize = scale / level;
 
 		game.splice(0, game.length, ...Array.from({"length": level * level}, _ => false));
 		cells.splice(0, cells.length, ...Array.from({"length": level * level}, (_, n) => rect({"x": (n % level) * cellSize + 1, "y": Math.floor(n / level) * cellSize + 1, "width": cellSize - 2, "height": cellSize - 2, "onclick": () => clicked(n)})));
