ec2a7965d4780af36d7486c4b712f091052cc26f
Michael Woolnough <michael.woolnough@gmail.com>
2023-10-12 07:21:15 +0100

    removed unnecessary comma

--- flipit/script.ts
+++ flipit/script.ts
@@ -11,7 +11,7 @@ ready.then(() => {
 	});
 	add("svg", {
 		"max-width": "100vw",
-		"max-height": "100vh",
+		"max-height": "100vh"
 	});
 	at("@media (prefers-color-scheme: light)", {
 		"html, body": {
