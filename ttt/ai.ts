d8a68a0a89c3f05fc9663e4380a5cc9af2c6967e
Michael Woolnough <michael.woolnough@gmail.com>
2023-10-21 21:23:45 +0100

    store moves as array for ease of access

--- ttt/ai.ts
+++ ttt/ai.ts
@@ -2,28 +2,27 @@ import {brain} from './brain.js';
 
 type Board = number;
 
-type Moves = number;
+type Moves = [number[], number[], number[], number[]];
 
 const b64 = [Array.from({"length": 26}, (_, n) => String.fromCharCode(65+n)), Array.from({"length": 26}, (_, n) => String.fromCharCode(97+n)), Array.from({"length": 10}, (_, n) => String.fromCharCode(48+n))].flat(),
       decode = (b: string) => (b64.indexOf(b.charAt(0)) << 18) | (b64.indexOf(b.charAt(1)) << 12) | (b64.indexOf(b.charAt(2)) << 6) | b64.indexOf(b.charAt(3)),
       boards = new Map<Board, Moves>();
 
 for (let i = 0; i < brain.length; i += 4) {
+	const moves: Moves = [[], [], [], []];
 	let b = decode(brain.slice(i, i+4)),
-	    board = 0,
-	    moves = 0;
+	    board = 0;
 
 	for (let p = 0; p < 9; p++) {
 		const r = b % 6;
 
 		b /= 6
 		board *= 4
-		moves *= 4
 
 		if (r >= 4) {
 			board |= r - 4;
 		} else {
-			moves |= r;
+			moves[r].push(p);
 		}
 	}
 
